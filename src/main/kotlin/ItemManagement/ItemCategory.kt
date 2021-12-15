62444d30e3e751f6a68a4ef14b7d661aa37722f5
akk1iii <alexzandre.dilag@gmail.com>
2021-12-15 11:33:16 +0800

    Added Categories

--- src/main/kotlin/ItemManagement/ItemCategory.kt
+++ src/main/kotlin/ItemManagement/ItemCategory.kt
@@ -1,4 +1,11 @@
 package ItemManagement
 
-enum class ItemCategory {
+enum class ItemCategory (shortName: String, longName: String) {
+
+    FROZEN ("FG","Frozen Goods"),
+    DAIRY ("DG","Dairy Goods"),
+    CANNED ("CG","Canned Goods"),
+    ALCOHOL ("AG","Alcohol Goods"),
+    CLEANING ("CP","Cleaning Products"),
+
 }
