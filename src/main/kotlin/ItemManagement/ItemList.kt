45b48c4c2db870e2e78ee6a3034bcd256b12112f
BLANK <48934075+QuickySilver@users.noreply.github.com>
2021-12-15 12:52:56 +0800

    Added the filter function for every category

--- src/main/kotlin/ItemManagement/ItemList.kt
+++ src/main/kotlin/ItemManagement/ItemList.kt
@@ -1,8 +1,14 @@
 package ItemManagement
 
-class ItemsList {
-    val items = mutableListOf<Item>()
+class ItemList () {
     init {
+
+    }
+    companion object {
+        const val buffer = 10
+        var items = mutableListOf<Item>()
+    }
+    fun generateItems () {
         items.add( Item("001A","TenderJuicy Hotdog", 20, ItemCategory.FROZEN))
         items.add( Item("001B","Pampanga's Best Tocino", 20, ItemCategory.FROZEN ))
         items.add( Item("001C","Purefoods Nuggers", 20, ItemCategory.FROZEN ))
@@ -19,4 +25,23 @@ class ItemsList {
         items.add( Item("005B","Tide ", 20, ItemCategory.CLEANING ))
         items.add( Item("005C","Zonrox", 20, ItemCategory.CLEANING ))
     }
+    /**
+     * This functions will filter every category in the itemList
+     */
+    fun frozenList (): List<Item> {
+        return items.filter { it.category == ItemCategory.FROZEN }
+    }
+    fun dairyList (): List<Item> {
+        return items.filter { it.category == ItemCategory.DAIRY }
+    }
+    fun cannedList (): List<Item> {
+        return items.filter { it.category == ItemCategory.CANNED }
+    }
+    fun alcoholList (): List<Item> {
+        return items.filter { it.category == ItemCategory.ALCOHOL }
+    }
+    fun cleaningList (): List<Item> {
+        return items.filter { it.category == ItemCategory.CLEANING }
+    }
+
 }
---
+++ src/main/kotlin/ItemManagement/ItemManagement.kt
@@ -0,0 +1,92 @@
+package ItemManagement
+
+import UserManagement.User
+
+class ItemManagement {
+
+    // This method gets the item from the item list by its name using a string parameter
+    private fun getItem(name: String): Item {
+        return ItemList.items.find { it.itemName.lowercase() == name.lowercase() }!!
+    }
+
+    //Method that would add specific number of stocks to a specific item
+    fun restockItem (itemName: String, quantity: Int ) {
+        val item = getItem(itemName)
+        item.stocks = item.stocks + quantity
+        println("$quantity piece(s) of ${item.itemName} has been restocked to the inventory")
+    }
+    //Method that would subtract specific number of stocks to a specific item
+    fun consumeItem (itemName: String, quantity: Int) {
+        val item = getItem(itemName)
+        if (item.stocks >= quantity) {
+            item.stocks = item.stocks - quantity
+            if (item.stocks < ItemList.buffer) {
+                println("$quantity piece(s) of ${item.itemName} has been checked out. There's only ${item.stocks} of ${item.itemName} left. Please restock immediately")
+            }
+            else {
+                println("$quantity piece(s) of ${item.itemName} has been checked out")
+            }
+        }
+        else {
+            throw IllegalArgumentException ("Insufficient Stocks")
+        }
+    }
+    //Method that would add specific item to the item list
+    fun registerItem (itemName: String, user: User) {
+        val item = getItem(itemName)
+        if (user.isAdmin) {
+            ItemList.items.add(item)
+            println("${item.itemName} is added to the inventory monitoring")
+        }
+        else {
+            throw IllegalAccessException ("You don't have the permission to do this")
+        }
+    }
+    //Method that would remove specific item to the item list
+    fun removeItem (itemName: String, user: User) {
+        val item = getItem(itemName)
+        if (user.isAdmin) {
+            ItemList.items.remove(item)
+            println("${item.itemName} has been removed to the inventory monitoring")
+        }
+        else {
+            throw IllegalAccessException ("You don't have the permission to do this")
+        }
+    }
+    fun displayFrozen () {
+        val frozenGoods = mutableMapOf<String, Int>()
+        ItemList().frozenList().forEach {
+            frozenGoods[it.itemName] = it.stocks
+        }
+        println(frozenGoods)
+    }
+    fun displayDairy () {
+        val dairy = mutableMapOf<String, Int>()
+        ItemList().dairyList().forEach {
+            dairy[it.itemName] = it.stocks
+        }
+        println(dairy)
+    }
+    fun displayCanned () {
+        val canned = mutableMapOf<String, Int>()
+        ItemList().cannedList().forEach {
+            canned[it.itemName] = it.stocks
+        }
+        println(canned)
+    }
+    fun displayAlcohol () {
+        val alcohol = mutableMapOf<String, Int>()
+        ItemList().alcoholList().forEach {
+            alcohol[it.itemName] = it.stocks
+        }
+        println(alcohol)
+    }
+    fun displayCleaning () {
+        val cleaning = mutableMapOf<String, Int>()
+        ItemList().cleaningList().forEach {
+            cleaning[it.itemName] = it.stocks
+        }
+        println(cleaning)
+    }
+
+}
